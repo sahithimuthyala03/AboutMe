cb4167beb778bdf3f50935498d89d541ffb19e19
sahithimuthyala03 <69254657+sahithimuthyala03@users.noreply.github.com>
2020-08-05 16:18:33 -0400

    Add files via upload

---
+++ AboutMe/AboutMe/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Sahithi Muthyala on 8/5/20.
+//  Copyright © 2020 Sahithi Muthyala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var funFact1: UILabel!
+    @IBOutlet weak var funFact2: UILabel!
+    @IBOutlet weak var funFact3: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    @IBAction func funFactDisplayButton(_ sender: UIButton) {
+        
+    }
+    
+    
+    
+}
